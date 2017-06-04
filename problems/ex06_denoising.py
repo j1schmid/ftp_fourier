844a41c222f4898ff59f5154d5ce81d9a6060a8f
Jonas <jonas@p8z68.home>
2017-06-04 10:05:14 +0200

    use signal from git repo, improve plot

--- problems/ex06_denoising.py
+++ problems/ex06_denoising.py
@@ -21,8 +21,11 @@ import os.path
 from numpy import genfromtxt
 
 def denoise(data,K,wname,level,threshold_type):
-	wavelet = pywt.Wavelet(wname)	
-	coeffs = pywt.wavedecn(data,wavelet,level=level)
+	wavelet = pywt.Wavelet(wname)
+	if level=='None':
+		coeffs = pywt.wavedecn(data,wavelet)
+	else:
+		coeffs = pywt.wavedecn(data,wavelet,level=level)
 	# estimate the standard deviation of the noise using the finest detail / nu
 	# coefficients
 	std_dev = np.median(np.abs(coeffs[-2]['d'])) / 0.6745
@@ -33,19 +36,18 @@ def denoise(data,K,wname,level,threshold_type):
 	coeffs = pywt.array_to_coeffs(arr, coeff_slices)
 	return (threshold, pywt.waverecn(coeffs,wavelet))
 
-def show_denoised(data,K,wname,level):
+def show_denoised(data,K,wname,level='None'):
 	threshold, dn_hard = denoise(data,K,wname,level,'hard')
-	_, dn_soft = denoise(data,K,'bior4.4',level,'soft')
+	_, dn_soft = denoise(data,K,wname,level,'soft')
 	print('threshold: {:f}'.format(threshold))
 	
-	fig = plt.figure()
-	graph = fig.add_subplot(111)
-	graph.plot(data)
-	graph.plot(dn_soft)
-	graph.plot(dn_hard)
-	plt.show()
+	plt.plot(dn_soft,label='\''+wname+'\' soft threshold')
+	plt.plot(dn_hard,label='\''+wname+'\' hard threshold')
+	return threshold
 
 def main():
+	fig = plt.figure()
+	
 	FILENAME='data/EEG1.MAT'
 	if os.path.isfile(FILENAME):
 		print('use file \'{:s}\' from fourier course'.format(FILENAME))
@@ -54,10 +56,19 @@ def main():
 	else:
 		FILENAME2='data/pam_signal.csv'
 		print('file \'{:s}\' from fourier course not found, use \'{:s}\' instead'.format(FILENAME,FILENAME2))
-		meas = genfromtxt(FILENAME2, delimiter=',')
+		
+		data = genfromtxt(FILENAME2, delimiter=',')
+		meas = data[:,1]
+		plt.plot(data[:,0],label='original') 
+		print('signal length is {:d}'.format(len(meas)))
+		
+	plt.plot(meas,label='measured signal')
+	threshold = show_denoised(meas,1.0,'haar')
+	#show_denoised(meas,1.0,'bior4.4',8)
 	
-	show_denoised(meas,1.0,'bior4.4',3)
-	show_denoised(meas,1.0,'bior4.4',8)
+	plt.legend(loc='best', borderaxespad=0.2)
+	plt.show()
+
 	#pdb.set_trace()
 
 if __name__ == '__main__':
